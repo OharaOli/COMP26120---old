47c079b6656f8d4ea30e8c6ae68889332c5e5626
Oliver <oliver.ohara@student.manchester.ac.uk>
2020-05-07 17:58:07 +0100

    Unkown compjudge error

--- elGamal.c
+++ elGamal.c
@@ -11,9 +11,9 @@
 // Task 1:
 // Highest common factor
 // Compute the highest common factor of a and b
-unsigned long hcf(unsigned long a, unsigned long b)
+unsigned long long hcf(unsigned long long a, unsigned long long b)
 {
-   unsigned long r;
+   unsigned long long r;
    if (b == 0)
    {
       return a;
@@ -31,9 +31,9 @@ unsigned long hcf(unsigned long a, unsigned long b)
 // Compute the function g^x mod p
 // The time complexity of this function is O(log x) because each time that x is
 // even we can divide it by two and this is the condition of the while loop
-unsigned long fme(unsigned long g, unsigned long x, unsigned long p)
+unsigned long long fme(unsigned long long g, unsigned long long x, unsigned long long p)
 {
-   unsigned long answer = 1;
+   unsigned long long answer = 1;
 
    while(x > 0)
    {
@@ -55,10 +55,10 @@ unsigned long fme(unsigned long g, unsigned long x, unsigned long p)
 // Inverse modulo prime
 // For a given y and p (prime) returns an x such that x*y = 1 mod p
 //
-unsigned long imp(unsigned long y, unsigned long p)
+unsigned long long imp(unsigned long long y, unsigned long long p)
 {
    // Check if an inverse exists
-   unsigned long check = hcf(y, p);
+   unsigned long long check = hcf(y, p);
    if (check != 1)
    {
       printf("\nInverse not possible for that input\n");
@@ -70,50 +70,50 @@ unsigned long imp(unsigned long y, unsigned long p)
    }
 } //imp
 
-void getPublicKey(unsigned long p, unsigned long g)
+void getPublicKey(unsigned long long p, unsigned long long g)
 {
    // Private key (x) (selected by user)
-   unsigned long x;
+   unsigned long long x;
    printf("\n----------------------------------------");
    printf("\nPlease select your private key: ");
-   scanf(" %lu", &x);
+   scanf(" %llu", &x);
 
    // Check that the value given lies between 1 and p
    while (x < 1 || x >= p)
    {
       printf("\n----------------------------------------");
       printf("\nYour private key must be between 1 and p, try again: ");
-      scanf(" %lu", &x);
+      scanf(" %llu", &x);
    } //while
 
    // Public key (y) (calculated using fme)
-   unsigned long y = fme(g, x, p);
-   printf("\nYour public key is: %lu", y);
+   unsigned long long y = fme(g, x, p);
+   printf("\nYour public key is: %llu", y);
    return;
 } //getPublicKey
 
 
-void encrypt(unsigned long p, unsigned long g)
+void encrypt(unsigned long long p, unsigned long long g)
 {
    // Message to encrypt (m) (selected by user)
-   unsigned long m;
+   unsigned long long m;
    printf("\n----------------------------------------");
    printf("\nPlease select number to send: ");
-   scanf(" %lu", &m);
+   scanf(" %llu", &m);
 
    // Check that the value given lies between 1 and p
    while (m < 1 || m >= p)
    {
       printf("\n----------------------------------------");
       printf("\nYour message must be between 1 and p, try again: ");
-      scanf(" %lu", &m);
+      scanf(" %llu", &m);
    } //while
 
    // Public key (y) (selected by user)
-   unsigned long y;
+   unsigned long long y;
    printf("\n----------------------------------------");
    printf("\nPlease type your public key: ");
-   scanf(" %lu", &y);
+   scanf(" %llu", &y);
 
    // Select random number (k) in the range 1 to p
    srand(time(0));
@@ -121,26 +121,26 @@ void encrypt(unsigned long p, unsigned long g)
    printf("\nk = %d", k);
 
    // Find the encrypted values a and b
-   unsigned long a = fme(g, k, p);
-   unsigned long b = m*fme(y, k, p);
+   unsigned long long a = fme(g, k, p);
+   unsigned long long b = m*fme(y, k, p);
 
    // Output the values for a and b
-   printf("\nThe encrypted message is: (%lu,%lu)", a, b);
+   printf("\nThe encrypted message is: (%llu,%llu)", a, b);
 } //encrypt
 
 
-void decrypt(unsigned long p, unsigned long g)
+void decrypt(unsigned long long p, unsigned long long g)
 {
    // String to hold the initial input for the encrypted message
-   char eMessage[100];
+   char eMessage[1000];
    char *token1;
    char *token2;
    // First part of the encrypted message (a) (entered by user)
-   unsigned long a;
+   unsigned long long a;
    // Second part of the encrypted message (b) (entered by user)
-   unsigned long b;
+   unsigned long long b;
    // Private key (x) (entered by user)
-   unsigned long x;
+   unsigned long long x;
 
    printf("\n----------------------------------------");
    //printf("\nPlease enter the first part of the message: ");
@@ -155,29 +155,29 @@ void decrypt(unsigned long p, unsigned long g)
    eMessage[strlen(eMessage)-1] = 0;
    token1 = strtok(eMessage, ",");
    token2 = strtok(NULL, ",");
-   a = strtoul(token1, NULL, 10);
-   b = strtoul(token2, NULL, 10);
+   a = strtoull(token1, NULL, 10);
+   b = strtoull(token2, NULL, 10);
 
    printf("\nPlease enter your private key: ");
-   scanf(" %lu", &x);
+   scanf(" %llu", &x);
 
    // Find the decrypted message
-   unsigned long decryptedMessage = (b / fme(a, x, p));
+   unsigned long long decryptedMessage = (b / fme(a, x, p));
 
    // Output the decrypted message
-   printf("The decrypted message is: %lu", decryptedMessage);
+   printf("The decrypted message is: %llu", decryptedMessage);
 } //decrypt
 
 int main(int argc, char *argv[])
 {
   // Prime modulus (p) (given)
-  unsigned long p = 65537;
+  unsigned long long p = 65537;
   // Primitive root (g) (given)
-  unsigned long g = 3;
+  unsigned long long g = 3;
   // Print p and g
   printf("\n----------------------------------------");
-  printf("\nPrime modulus is %lu", p);
-  printf("\nPrimitive root wrt %lu is %lu", p, g);
+  printf("\nPrime modulus is %llu", p);
+  printf("\nPrimitive root wrt %llu is %llu", p, g);
 
   //Give the user a choice of functions and read input
   char choice;
