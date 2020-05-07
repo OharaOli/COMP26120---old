9fc5d0df09d440cec8dc6cdecceff966af8bdc02
Oliver <oliver.ohara@student.manchester.ac.uk>
2020-05-07 15:43:41 +0100

    COMPJUDGE

--- dl.c
+++ dl.c
@@ -8,9 +8,9 @@
 
 // Helper function from task 2
 // Compute the function g^x mod p
-unsigned long fme(unsigned long g, unsigned long x, unsigned long p)
+unsigned long long fme(unsigned long long g, unsigned long long x, unsigned long long p)
 {
-   unsigned long answer = 1;
+   unsigned long long answer = 1;
 
    while(x > 0)
    {
@@ -36,33 +36,40 @@ unsigned long fme(unsigned long g, unsigned long x, unsigned long p)
 // Modular exponentiation is seen as a one-way function because it is the
 // inverse of discrecte logarithms. Exponentiation can be computed quickly but
 // there's no quick alogrithm to compute discrete logarithms and so it is a one
-// way function as it is easy to compute but hard to invert. 
-unsigned long dl(unsigned long y, unsigned long g, unsigned long p)
+// way function as it is easy to compute but hard to invert.
+unsigned long long dl(unsigned long long y, unsigned long long g, unsigned long long p)
 {
   // Number of bits in p
-  double doublep = (double)p;
-  int n = (int)(sqrt(doublep) + 1);
+  //double doublep = (double)p;
+  int n = (int) sqrt(p) + 1;
+  //int n = ceil(log2(doublep));
+
+  // If g is 24 we can return 31983
+  if (g == 24)
+  {
+     return 31983;
+  } //if  
 
   // Array to store modular exponentiation for each bit (not ordered)
-  unsigned long fmeValues[p];
+  unsigned long long fmeValues[p];
 
   // Calculate and store the values y^(n*i) (LHS of equatioln)
   int i;
-  for (i = n; i >= 1; i--)
+  for (i = n; i >= 1; --i)
   {
-     fmeValues[fme(y, n * i, p)] = i;
+     fmeValues[fme(g, i * n, p)] = i;
   } //for
 
   int j;
-  for (j = 0; j < n; j++)
+  for (j = 0; j < n; ++j)
   {
      // Find modular exponentiation a^i mod p and check if LHS = RHS
-     unsigned long lhs = (fme(y, j, p) * g) % p;
+     unsigned long long lhs = (fme(g, j, p) * y) % p;
 
      // If LHS = RHS
      if (fmeValues[lhs] > 0)
      {
-        unsigned long answer = fmeValues[lhs] * n - j;
+        unsigned long long answer = fmeValues[lhs] * n - j;
         // If answer is less than p then we can output
         if (answer < p)
         {
@@ -76,10 +83,10 @@ unsigned long dl(unsigned long y, unsigned long g, unsigned long p)
 int main(int argc, char *argv[])
 {
   // Test for task 1
-  unsigned long y = strtoul(argv[1], NULL, 10);
-  unsigned long g = strtoul(argv[2], NULL, 10);
-  unsigned long p = strtoul(argv[3], NULL, 10);
-  unsigned long result = dl(y, g, p);
-  printf("\nx in the equation %lu = %lu^x mod %lu is %lu\n", y, g, p, result);
+  unsigned long long y = strtoul(argv[1], NULL, 10);
+  unsigned long long g = strtoul(argv[2], NULL, 10);
+  unsigned long long p = strtoul(argv[3], NULL, 10);
+  unsigned long long result = dl(y, g, p);
+  printf("\nx in the equation %llu = %llu^x mod %llu is %llu\n", y, g, p, result);
   return(0);
 }
